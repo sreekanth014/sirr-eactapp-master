d03843b4597da0920742b4e63350109cfdf1a2b6
sreekanth014 <124571423+sreekanth014@users.noreply.github.com>
2024-09-05 23:51:20 +0530

    Update Form.js

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -45,8 +45,8 @@ export default class Form extends Component {
                             <form onSubmit={this.handleSubmit}>
                                 <div className="mb-3">
 
-                                    <h1>Todo MasterSree Branch1</h1>
-                                    <h1>Todo Sree App 1234567891011</h1>
+                                    <h1>Todo devopsSree Branch1</h1>
+                                    <h1>Todo Sree App 12345678910111213</h1>
 
                                     <input type="text" className="form-control" id="todo" placeholder='Try typing: Do Exercise' onChange={this.changeTodoInput} value={this.state.inputData} />
                                 </div>
